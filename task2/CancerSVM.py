4ce22bfc1a022c9d3c5f897ca3698a2eb667ce9e
Zenoss <kumaraayan763@gmail.com>
2026-02-14 22:48:26 +0530

    Create CancerSVM.py

---
+++ task2/CancerSVM.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+df=pd.read_csv("gene_expression.csv")
+x=df.drop(columns=['Cancer Present'])
+y=df['Cancer Present']
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
+svm=SVC(kernel="rbf")
+svm.fit(x_train, y_train)
+print("RBF kernel SVM accuracy: ",svm.score(x_test,y_test))
+svm2=SVC(kernel="linear")
+svm2.fit(x_train,y_train)
+print("Linear SVM accuracy: ",svm2.score(x_test,y_test))
